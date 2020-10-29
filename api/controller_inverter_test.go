fd6cf476d58cb34b29eab79de5c504ba71b90bb6
Rogerio <rogerioalves.ee@gmail.com>
2020-10-29 11:28:33 -0300

    Grouping inverter equality assertion

--- api/controller_inverter_test.go
+++ api/controller_inverter_test.go
@@ -3,26 +3,14 @@ package api
 import (
 	"context"
 	"encoding/json"
-	"io"
 	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/rjmalves/cpid-solar-gateway/api/models"
 	"github.com/rjmalves/cpid-solar-gateway/api/seed"
 	"github.com/stretchr/testify/assert"
 )
-
-func makeRequest(r http.Handler, method, path string, body io.Reader) (*httptest.ResponseRecorder, error) {
-	req, err := http.NewRequest(method, path, body)
-	if err != nil {
-		return nil, err
-	}
-	rr := httptest.NewRecorder()
-	r.ServeHTTP(rr, req)
-	return rr, nil
-}
 
 func TestAPIv1ListEmptyInverters(t *testing.T) {
 	ctx := context.Background()
@@ -75,18 +63,7 @@ func TestAPIv1ListInverters(t *testing.T) {
 	}
 	assert.Equal(t, len(invs), len(data))
 	for i := range invs {
-		assert.Equal(t, invs[i].ID, data[i].ID)
-		assert.Equal(t, invs[i].Serial, data[i].Serial)
-		assert.Equal(t, invs[i].Power, data[i].Power)
-		assert.Equal(t, invs[i].Voltage, data[i].Voltage)
-		assert.Equal(t, invs[i].Frequency, data[i].Frequency)
-		assert.Equal(t, invs[i].Communication, data[i].Communication)
-		assert.Equal(t, invs[i].Status, data[i].Status)
-		assert.Equal(t, invs[i].Switch, data[i].Switch)
-		assert.Equal(t, invs[i].EnergyToday, data[i].EnergyToday)
-		assert.Equal(t, invs[i].EnergyThisMonth, data[i].EnergyThisMonth)
-		assert.Equal(t, invs[i].EnergyThisYear, data[i].EnergyThisYear)
-		assert.Equal(t, invs[i].TotalEnergy, data[i].TotalEnergy)
+		checkInverters(t, *invs[i], data[i])
 	}
 }
 
@@ -141,16 +118,5 @@ func TestAPIv1GetInverterBySerialSuccess(t *testing.T) {
 		t.Errorf("Error while reading inverter from DB: %v\n", err)
 		return
 	}
-	assert.Equal(t, i.ID, data.ID)
-	assert.Equal(t, i.Serial, data.Serial)
-	assert.Equal(t, i.Power, data.Power)
-	assert.Equal(t, i.Voltage, data.Voltage)
-	assert.Equal(t, i.Frequency, data.Frequency)
-	assert.Equal(t, i.Communication, data.Communication)
-	assert.Equal(t, i.Status, data.Status)
-	assert.Equal(t, i.Switch, data.Switch)
-	assert.Equal(t, i.EnergyToday, data.EnergyToday)
-	assert.Equal(t, i.EnergyThisMonth, data.EnergyThisMonth)
-	assert.Equal(t, i.EnergyThisYear, data.EnergyThisYear)
-	assert.Equal(t, i.TotalEnergy, data.TotalEnergy)
+	checkInverters(t, i, data)
 }
--- api/server_test.go
+++ api/server_test.go
@@ -1,10 +1,41 @@
 package api
 
 import (
+	"io"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
+
+	"github.com/rjmalves/cpid-solar-gateway/api/models"
+	"github.com/stretchr/testify/assert"
 )
+
+func makeRequest(r http.Handler, method, path string, body io.Reader) (*httptest.ResponseRecorder, error) {
+	req, err := http.NewRequest(method, path, body)
+	if err != nil {
+		return nil, err
+	}
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
+	return rr, nil
+}
+
+func checkInverters(t *testing.T, inv1, inv2 models.Inverter) {
+	assert.Equal(t, inv1.ID, inv2.ID)
+	assert.Equal(t, inv1.Serial, inv2.Serial)
+	assert.Equal(t, inv1.Power, inv2.Power)
+	assert.Equal(t, inv1.Voltage, inv2.Voltage)
+	assert.Equal(t, inv1.Frequency, inv2.Frequency)
+	assert.Equal(t, inv1.Communication, inv2.Communication)
+	assert.Equal(t, inv1.Status, inv2.Status)
+	assert.Equal(t, inv1.Switch, inv2.Switch)
+	assert.Equal(t, inv1.EnergyToday, inv2.EnergyToday)
+	assert.Equal(t, inv1.EnergyThisMonth, inv2.EnergyThisMonth)
+	assert.Equal(t, inv1.EnergyThisYear, inv2.EnergyThisYear)
+	assert.Equal(t, inv1.TotalEnergy, inv2.TotalEnergy)
+}
 
 func TestMain(m *testing.M) {
 
